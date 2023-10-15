7a8ed2801b87834975d8ee4f2ccc35c28226424a
fr0staman <mrfrostaman@gmail.com>
2023-10-15 17:30:22 +0300

    Possible date generation optimize

--- src/utils/date.rs
+++ src/utils/date.rs
@@ -2,23 +2,23 @@ use chrono::{prelude::*, Duration};
 
 const FIXED_HOUR: u32 = 12;
 const FIXED_MINUTE: u32 = 36;
-const FIXED_OFFSET: i32 = -3 * 3600;
+const FIXED_OFFSET_IN_SECONDS: i32 = -3 * 3600;
+const FIXED_OFFSET: FixedOffset =
+    match FixedOffset::east_opt(FIXED_OFFSET_IN_SECONDS) {
+        Some(offset) => offset,
+        None => panic!("Wrong fixed offset!"),
+    };
 
 pub fn get_datetime() -> NaiveDateTime {
     let datetime = Utc::now().naive_utc();
-    let tz_offset = get_offset();
-    let date = tz_offset.from_local_datetime(&datetime).unwrap();
+
+    let date = FIXED_OFFSET.from_local_datetime(&datetime).unwrap();
 
     date.naive_utc()
 }
 
 pub fn get_date() -> NaiveDate {
     get_datetime().date()
-}
-
-#[allow(unused)]
-pub fn get_timestamp() -> i64 {
-    get_datetime().timestamp()
 }
 
 pub fn get_fixed_timestamp(expected_datetime: NaiveDateTime) -> i64 {
@@ -41,8 +41,4 @@ pub fn get_timediff(cur_datetime: NaiveDateTime) -> (i64, i64, i64) {
     let minutes = duration.num_minutes() % 60;
     let seconds = duration.num_seconds() % 60;
     (hours, minutes, seconds)
-}
-
-pub fn get_offset() -> FixedOffset {
-    FixedOffset::east_opt(FIXED_OFFSET).unwrap()
 }
