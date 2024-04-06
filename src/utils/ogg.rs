d2ffacaa451b1e28b9a0b779331eb4680117c722
fr0staman <mrfrostaman@gmail.com>
2024-04-06 21:53:35 +0300

    feat: improve encoding, decoding, parallel sound increasing with rayon

--- src/utils/ogg.rs
+++ src/utils/ogg.rs
@@ -1,27 +1,51 @@
 use std::io::Cursor;
 
+use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+
 pub async fn increase_sound(
     input_data: bytes::Bytes,
     volume_factor: f32,
 ) -> Option<Vec<u8>> {
-    let mut raw = tokio::task::spawn_blocking(move || {
-        let (raw, _header) =
-            ogg_opus::decode::<_, 48000>(Cursor::new(input_data)).ok()?;
-        Some(raw)
-    })
-    .await
-    .ok()??;
+    let raw = _decode(input_data).await?;
 
-    let raw = tokio::task::spawn_blocking(move || {
-        for num in &mut raw {
-            *num = (*num as f32 * volume_factor) as i16;
-        }
-        raw
-    })
-    .await
-    .ok()?;
+    let raw = _increase(raw, volume_factor).await?;
 
-    tokio::task::spawn_blocking(move || ogg_opus::encode::<48000, 1>(&raw).ok())
-        .await
-        .ok()?
+    _encode(raw).await
+}
+
+async fn _decode(input_data: bytes::Bytes) -> Option<Vec<i16>> {
+    let (send, recv) = tokio::sync::oneshot::channel();
+
+    rayon::spawn(move || {
+        let decode_res =
+            ogg_opus::decode::<_, 48000>(Cursor::new(input_data)).ok();
+
+        let _ = send.send(decode_res.map(|v| v.0));
+    });
+
+    recv.await.ok()?
+}
+
+async fn _increase(mut raw: Vec<i16>, volume_factor: f32) -> Option<Vec<i16>> {
+    let (send, recv) = tokio::sync::oneshot::channel();
+
+    rayon::spawn(move || {
+        raw.par_iter_mut()
+            .for_each(|v| *v = (*v as f32 * volume_factor) as i16);
+
+        let _ = send.send(raw);
+    });
+
+    recv.await.ok()
+}
+
+async fn _encode(raw: Vec<i16>) -> Option<Vec<u8>> {
+    let (send, recv) = tokio::sync::oneshot::channel();
+
+    rayon::spawn(move || {
+        let encoded = ogg_opus::encode::<48000, 1>(&raw).ok();
+        let _ = send.send(encoded);
+    });
+
+    recv.await.ok()?
 }
