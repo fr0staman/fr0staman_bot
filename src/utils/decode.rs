131f9b1af966b51ebdab30b81079965b2adcc837
fr0staman <mrfrostaman@gmail.com>
2025-09-20 21:36:57 +0300

    refactor: migrate from `packed_struct` to `byteorder`

--- src/utils/decode.rs
+++ src/utils/decode.rs
@@ -1,52 +1,37 @@
-use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
-use packed_struct::prelude::*;
+use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD};
+use byteorder::{LittleEndian, ReadBytesExt};
 
 const ID32_FORMAT_SIZE: usize = 27;
 
-#[derive(PackedStruct, Debug)]
-pub struct MessageData32 {
-    #[packed_field(endian = "lsb")]
-    dc_id: i32,
-    #[packed_field(endian = "lsb")]
-    message_id: i32,
-    #[packed_field(endian = "lsb")]
-    chat_id: i32,
-    #[packed_field(endian = "lsb")]
-    access_hash: i64,
-}
-
-#[derive(PackedStruct, Debug)]
+#[allow(dead_code)]
+#[derive(Debug)]
 pub struct MessageData {
-    #[packed_field(endian = "lsb")]
     pub dc_id: i32,
-    #[packed_field(endian = "lsb")]
     pub chat_id: i64,
-    #[packed_field(endian = "lsb")]
     pub message_id: i32,
-    #[packed_field(endian = "lsb")]
     pub access_hash: i64,
 }
 
 pub fn decode_inline_message_id(
     inline_message_id: &str,
 ) -> Option<MessageData> {
+    let is_i32_chat = inline_message_id.len() == ID32_FORMAT_SIZE;
     let decoded_inline_message_id =
         URL_SAFE_NO_PAD.decode(inline_message_id).ok()?;
 
-    if inline_message_id.len() == ID32_FORMAT_SIZE {
-        let data = MessageData32::unpack_from_slice(&decoded_inline_message_id)
-            .ok()?;
-        // Force message data for simplicity
-        MessageData {
-            dc_id: data.dc_id,
-            chat_id: data.chat_id as i64,
-            message_id: data.message_id,
-            access_hash: data.access_hash,
-        }
+    let mut cursor = std::io::Cursor::new(decoded_inline_message_id);
+
+    let dc_id = cursor.read_i32::<LittleEndian>().ok()?;
+    let message_id = cursor.read_i32::<LittleEndian>().ok()?;
+    let chat_id = if is_i32_chat {
+        cursor.read_i32::<LittleEndian>().ok()? as i64
     } else {
-        MessageData::unpack_from_slice(&decoded_inline_message_id).ok()?
-    }
-    .into()
+        cursor.read_i64::<LittleEndian>().ok()?
+    };
+    let access_hash = cursor.read_i64::<LittleEndian>().ok()?;
+
+    // Force message data for simplicity
+    Some(MessageData { dc_id, chat_id, message_id, access_hash })
 }
 
 // https://github.com/teloxide/teloxide/blob/ae0451f7d72e78fdeb317db397fb602a29eda17a/crates/teloxide-core/src/types/chat_id.rs#L107
